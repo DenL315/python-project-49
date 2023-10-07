bc979ccbda03646a96ce0d585b9f21f064353ee4
DenL315 <143326142+DenL315@users.noreply.github.com>
2023-10-07 15:13:34 +0000

    Step 6, update Step 5

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DenL315/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/DenL315/python-project-49/actions)
 https://asciinema.org/a/hK0BwGrTOkuGdM6L5WuBwNcIz 
+https://asciinema.org/a/KH2rikDiiriRAKUW4h0yYQUM6 
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
-def brain_even():
+def brain_calc():
     import prompt, random
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('What is the result of the expression?')
     counter = 0
+    calc_list = ['+', '-','*']
     while counter < 3:
-        a = random.randint(0, 100)
-        if a % 2 == 0:
-            correct_answer = 'yes'
+        a, b = random.randint(0, 100), random.randint(0, 100)
+        c = random.choice(calc_list)
+        if c == '+':
+            correct_answer = str(a + b)
+        elif c == '-':
+            correct_answer = str(a - b)
         else:
-            correct_answer = 'no'
-        test1 = prompt.string(f'Question: {a}\nYour answer: ')
+            correct_answer = str(a * b)
+        test1 = prompt.string(f'Question: {a} {c} {b}\nYour answer: ')
         if test1 == correct_answer:
             print('Correct!')
             counter += 1
@@ -22,9 +26,8 @@ def brain_even():
             print(f"'{test1}' is wrong answer ;(. Correct answer is '{correct_answer}'.\nLet's try again, {name}!")
             break
 
-
 def main():
-    brain_even()
+    brain_calc()
 
 
 if __name__ == '__main__':
